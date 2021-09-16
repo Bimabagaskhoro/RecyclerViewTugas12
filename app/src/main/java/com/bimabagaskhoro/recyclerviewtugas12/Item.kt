4a429307bdac99315dfe00cb28007b62a4de90b2
Bima Bagaskhoro <bimabagas644@gmail.com>
2021-09-16 16:40:52 +0700

    first commit

---
+++ app/src/main/java/com/bimabagaskhoro/recyclerviewtugas12/Item.kt
@@ -0,0 +1,7 @@
+package com.bimabagaskhoro.recyclerviewtugas12
+
+data class Item(
+    var name: String,
+    var price: String,
+    var avatar: String
+)
---
+++ app/src/main/java/com/bimabagaskhoro/recyclerviewtugas12/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.bimabagaskhoro.recyclerviewtugas12
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bimabagaskhoro.recyclerviewtugas12.databinding.ActivityMainBinding
+import java.util.ArrayList
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val actionbar = supportActionBar
+        actionbar!!.title = getString(R.string.home)
+
+        val adapter = ItemAdapter(getlistItem())
+        
+        binding.apply {
+            rvItem.layoutManager = LinearLayoutManager(this@MainActivity)
+            rvItem.setHasFixedSize(true)
+            rvItem.adapter = adapter
+        }
+    }
+
+    private fun getlistItem(): ArrayList<Item> {
+        val name = resources.getStringArray(R.array.name)
+        val price = resources.getStringArray(R.array.price)
+        val avatar = resources.getStringArray(R.array.avatar)
+
+        val listItem = ArrayList<Item>()
+        for (i in name.indices) {
+            val item = Item(
+                name[i],
+                price[i],
+                avatar[i]
+            )
+            listItem.add(item)
+        }
+        return listItem
+    }
+}
